ecebf8b4529f941840fef854bbaf68c5e1fac102
Timothy Totten <2010@totten.ca>
2024-09-13 14:00:59 -0700

    better now

--- bin/build.js
+++ bin/build.js
@@ -5,7 +5,42 @@
 const process = require('node:process');
 const cwd = process.cwd();
 const md = require('../lib/meta-docs');
-const rules = require(cwd+'/lum.build');
+const Builder = require('../lib/rule-builder');
+const {F,isObj} = require('@lumjs/core/types/basics');
+const rulesConf = require(cwd+'/lum.build');
 
-md.mkdirs(rules);
-md.docFiles(rules);
+let rules, retval = null;
+
+if (typeof rulesConf === F)
+{
+  rules = new Builder();
+  retval = rulesConf.call(md, rules);
+}
+else if (isObj(rulesConf))
+{ 
+  if (typeof rulesConf.run === F)
+  {
+    rules = new Builder(rulesConf);
+    retval = rulesConf.run(rules, md);
+  }
+  else
+  { // Fall back on the conf being the rules
+    rules = rulesConf;
+  }
+}
+else
+{
+  console.error({rulesConf});
+  throw new TypeError("Invalid export from lum.build.js");
+}
+
+if (rules.debug)
+{
+  console.debug({rules, retval, files: rules.files});
+}
+
+if (!rules.done)
+{
+  md.runAll(isObj(retval) ? retval : rules);
+}
+
